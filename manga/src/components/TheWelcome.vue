1221a24a855880f33091f3738c6998b9a82bd0b5
Dsaquel <noblet.ouwaysgta5@gmail.com>
2022-03-07 14:46:04 +0100

    first commit

---
+++ manga/src/components/TheWelcome.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="container">
+    <div v-for="article in articles" class="article" :key="article">
+      <h4>{{ article.about }}</h4>
+      <p>{{ article.price }} $</p>
+      <v-btn @click="getArticle(article.about, article.price)"
+        >Ajouter au panier</v-btn
+      >
+    </div>
+  </div>
+  <h2 v-if="recapArticle.length > 0">
+    Article ajouter :
+    <strong v-for="article in recapArticle" :key="article"
+      >{{ article.about
+      }}<strong v-if="article.number !== 0">({{ article.number }})</strong>,
+    </strong>
+  </h2>
+  <h1>{{ totalPrice }} e</h1>
+  <div v-if="recapArticle.length > 0" class="container">
+    <div
+      class="article"
+      v-for="articleAdded in recapArticle"
+      :key="articleAdded"
+    >
+      <h4>{{ articleAdded.about }}</h4>
+      <small class="floatTopRight"> quantité : {{ articleAdded.number }}</small>
+      <p>{{ articleAdded.price }} $</p>
+      <small v-show="false">{{ articleAdded.id }}</small>
+      <v-btn
+        @click="
+          deleteArticle(
+            articleAdded.id,
+            articleAdded.price,
+            articleAdded.number
+          )
+        "
+      >
+        Retirer article
+      </v-btn>
+    </div>
+  </div>
+
+  <div class="createArticle">
+    <h3>Creer un article !</h3>
+    <input type="text" v-model="newName" required />
+    <input type="number" v-model="newPrice" required />
+    <v-btn @click="createArticle()">Creer</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    let id = 0;
+    return {
+      articles: [
+        { id: id++, about: "Brosse à dent", price: 3, number: 1 },
+        { id: id++, about: "Sèche cheveux", price: 53, number: 1 },
+        { id: id++, about: "babybel x20", price: 4, number: 1 },
+        { id: id++, about: "Ordinateur puissant", price: 1200, number: 1 },
+      ],
+      secondId: 0,
+      recapArticle: [],
+      totalPrice: 0,
+      newName: "Micro",
+      newPrice: 50,
+      numberOfArticle: 0,
+    };
+  },
+  watch: {
+    "recapArticle.length": function () {},
+  },
+  methods: {
+    getArticle: function (name, price) {
+      let isNameMatched = this.recapArticle.find(function (article) {
+        return article.about === name;
+      });
+      if (isNameMatched) {
+        isNameMatched.number++;
+      } else {
+        this.recapArticle.push({
+          id: this.secondId++,
+          about: name,
+          price: price,
+          number: 1,
+        });
+      }
+      this.totalPrice += Number(price);
+    },
+    deleteArticle: function (id, price, numberLeft) {
+      if (numberLeft > 1) {
+        const article = this.recapArticle.find(article => article.id == id)
+        article.number--
+      } else {
+        //removing article from DOM
+        this.recapArticle = this.recapArticle.filter((arr) => arr.id != id);
+      }
+      this.totalPrice -= Number(price);
+    },
+    createArticle: function () {
+      this.articles.push({
+        id: this.secondId++,
+        about: this.newName,
+        price: this.newPrice,
+        number: 0,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  overflow: unset;
+}
+.article {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-width: 5px;
+  border-style: solid;
+  border-top-color: #5271ff;
+  border-bottom-color: #ffbd59;
+  border-left-color: #5271ff;
+  border-right-color: #ffbd59;
+  text-align: center;
+  margin: 5px;
+  border-radius: 10px;
+}
+
+.createArticle {
+  margin: auto;
+  width: min-content;
+  padding: 10px;
+  border: 2px solid black;
+}
+.floatTopRight {
+  display: inline-block;
+  right: 0;
+}
+
+h1 {
+  text-align: center;
+}
+input[type="number"],
+input[type="text"] {
+  background-color: #5271ff;
+  margin: 5px;
+  padding: 4px;
+}
+
+input:focus {
+  color: #fff;
+  background-color: blue;
+  border: 2px solid #fff;
+}
+</style>
---
+++ manga/src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <WelcomeItem/>
+  </div>
+</template>
+
+<script>
+import WelcomeItem from '../components/WelcomeItem.vue';
+
+export default {
+  name: 'Home',
+  
+  components: {
+    WelcomeItem,
+  },
+};
+</script>
