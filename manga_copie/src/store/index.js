a5583c215266bd66dd0a20bf4e21dbefd1754430
Dsaquel <noblet.ouwaysgta5@gmail.com>
2022-03-16 23:33:46 +0100

    more performance efficient

--- manga_copie/src/store/index.js
+++ manga_copie/src/store/index.js
@@ -29,17 +29,20 @@ export default new Vuex.Store({
   mutations: {
     // TODO: put data of user temporaly in vuex & create user!!
     setUserConnected (state, payload) {
-      if (state.fakeDatabaseOfUser.email === payload.email && state.fakeDatabaseOfUser.password === payload.password) {
-        state.user.id = 1
-        state.user.email = payload.email
-        state.user.password = payload.password
-      }
+      state.user.id = 1
+      state.user.email = payload.email
+      state.user.password = payload.password
     }
   },
   actions: {
     // TODO: check db if user exist
-    async checkUserExist ({ commit }, payload) {
-      commit('setUserConnected', payload)
+    async checkUserExist ({
+      commit,
+      state
+    }, payload) {
+      if (state.fakeDatabaseOfUser.email === payload.email && state.fakeDatabaseOfUser.password === payload.password) {
+        commit('setUserConnected', payload)
+      }
     }
   },
   modules: {
