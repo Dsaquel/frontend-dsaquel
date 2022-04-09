bf6264d6f1a043f1a6ffb62c9d99cb7206646d91
Dsaquel <noblet.ouwaysgta5@gmail.com>
2022-04-09 23:38:57 +0200

    wip

--- manga_copie/src/App.vue
+++ manga_copie/src/App.vue
@@ -7,7 +7,7 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn >
+      <v-btn @click="dialog = true">
         <v-icon class="mr-2">mdi-account-circle</v-icon>
         Se connecter
       </v-btn>
@@ -43,6 +43,138 @@
     </v-navigation-drawer>
 
     <v-main>
+      <v-dialog
+        style="transform-origin: center center"
+        persistent
+        v-model="dialog"
+        max-width="500"
+      >
+        <v-card v-show="index">
+          <v-card-title class="flex-nowrap align-start">
+            {{ etatStep }}
+            <v-spacer></v-spacer>
+            <v-btn @click="dialog = false" icon
+              ><v-icon>mdi-close</v-icon></v-btn
+            >
+          </v-card-title>
+          <v-divider></v-divider>
+          <v-card-text>
+            <v-container>
+              <v-window v-model="step">
+                <v-window-item value="index">
+                  <div
+                    class="d-flex flex-column justify-space-between"
+                    style="min-height: 100%"
+                  ></div>
+                  <p class="mb-5 font-weight-medium grey--text text--darken-1">
+                    Login
+                  </p>
+                  <v-form v-model="valid" ref="form" lazy-validation>
+                    <v-text-field
+                      v-model="email"
+                      :rules="emailRules"
+                      label="E-mail"
+                      required
+                    ></v-text-field>
+
+                    <v-text-field
+                      v-model="password"
+                      type="password"
+                      :rules="passwordRules"
+                      label="password"
+                      required
+                    ></v-text-field>
+
+                    <v-card-actions class="justify-space-between">
+                      <div
+                        @click="step = 'resetPassword'"
+                        style="cursor: pointer"
+                        class="
+                          app-link
+                          text-decoration-none
+                          primary--text
+                          font-weight-medium
+                          d-inline-block
+                        "
+                        replace
+                      >
+                        forgot password ?
+                      </div>
+                      <v-btn
+                        :disabled="!valid"
+                        color="success"
+                        class="mr-4"
+                        @click="validate"
+                        >login</v-btn
+                      >
+                    </v-card-actions>
+                  </v-form>
+                  <v-divider></v-divider>
+                  <div class="d-flex flex-column text-center body-2">
+                    <span class="my-5">No account ?</span>
+                    <v-btn @click="step = 'createAccount'" color="warning"
+                      >create account</v-btn
+                    >
+                  </div>
+                </v-window-item>
+                <v-window-item value="createAccount">
+                  <v-form ref="form" v-model="valid">
+                    <v-text-field
+                      v-model="email"
+                      :rules="emailRules"
+                      label="E-mail"
+                      required
+                    ></v-text-field>
+                    <v-text-field
+                      v-model="password"
+                      type="password"
+                      :rules="passwordRules"
+                      label="Mot de passe"
+                      required
+                    ></v-text-field>
+                    <v-text-field
+                      v-model="pseudo"
+                      :rules="pseudoRules"
+                      :counter="32"
+                      label="Pseudo"
+                      required
+                    ></v-text-field>
+                  </v-form>
+                  <v-card-actions class="justify-end px-0 pb-0">
+                    <v-btn @click="step = 'index'">back off</v-btn>
+                    <v-btn color="warning" @click="validate">Register</v-btn>
+                  </v-card-actions>
+                </v-window-item>
+                <v-window-item value="resetPassword">
+                  <v-form v-model="valid">
+                    <v-col>
+                      <v-row align="center">
+                        <v-text-field
+                          v-model="email"
+                          :rules="emailRules"
+                          label="E-mail"
+                          required
+                        ></v-text-field>
+                      </v-row>
+                    </v-col>
+                    <v-card-actions class="justify-end px-0 pb-0">
+                      <v-btn @click="step = 'index'">back off</v-btn>
+                      <v-btn
+                        :disabled="!valid"
+                        color="success"
+                        class="mr-4"
+                        @click="validate"
+                      >
+                        Send
+                      </v-btn>
+                    </v-card-actions>
+                  </v-form>
+                </v-window-item>
+              </v-window>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
       <router-view :key="$route.path" />
     </v-main>
   </v-app>
@@ -58,21 +190,62 @@ export default {
         { name: 'Anime', to: '/anime' },
         { name: 'Manga', to: '/manga' }
       ],
+      index: 1,
+      step: 'index',
       tab: null,
       isUserConnected: true,
-      isOpened: false
+      isOpened: false,
+      valid: false,
+      email: '',
+      dialog: false,
+      password: '',
+      passwordRules: [
+        (v) => !!v || 'Mot de passe requis',
+        (v) => v.length >= 4 || 'Mot de passe supérieur à 8 caractères'
+      ],
+      emailRules: [
+        (v) => !!v || 'E-mail requis',
+        (v) => /.+@.+/.test(v) || 'E-mail doit etre valide'
+      ],
+      pseudo: '',
+      pseudoRules: [
+        (v) => !!v || 'Pseudo requis',
+        (v) => v.length <= 32 || 'Pseudo inférieur à 32 caractères'
+      ]
     }
   },
-  methods: {},
+  methods: {
+    validate () {
+      this.$refs.form.validate()
+      const payload = { email: this.email, password: this.password }
+      this.$store.dispatch('checkUserExist', payload)
+    },
+    dialogSystem (open, close) {
+      this.close = false
+    }
+  },
   computed: {
     menus () {
-      const menus = [
-        { title: 'Suggestion', icon: 'mdi-gavel' }
-      ]
+      const menus = [{ title: 'Suggestion', icon: 'mdi-gavel' }]
       if (this.isUserConnected) {
-        menus.push({ title: 'Account', icon: 'mdi-account-box' }, { title: 'Library', icon: 'mdi-view-dashboard' })
+        menus.push(
+          { title: 'Account', icon: 'mdi-account-box' },
+          { title: 'Library', icon: 'mdi-view-dashboard' }
+        )
       }
       return menus
+    },
+    etatStep () {
+      switch (this.step) {
+        case 'index':
+          return 'Sign-up'
+        case 'createAccount':
+          return 'Create account'
+        case 'resetPassword':
+          return 'Reset password'
+        default:
+          return 'sign-up'
+      }
     }
   }
 }
--- manga_copie/src/components/login/LoginComponent.vue
+++ manga_copie/src/components/login/LoginComponent.vue
@@ -1,57 +1,3 @@
-<template>
-  <v-card>
-    <v-card-title class="flex-nowrap align-start">
-      Connexion
-      <v-spacer></v-spacer>
-      <v-btn icon><v-icon>mdi-close</v-icon></v-btn>
-    </v-card-title>
-    <v-divider></v-divider>
-    <v-card-text>
-      <v-container>
-        <div class="d-flex flex-column justify-space-between" style="min-height: 100%;"></div>
-        <p class="mb-5 font-weight-medium grey--text text--darken-1">Login</p>
-        <v-form v-model="valid" ref="form" lazy-validation>
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            required
-          ></v-text-field>
-
-          <v-text-field
-            v-model="password"
-            type="password"
-            :rules="passwordRules"
-            label="Mot de passe"
-            required
-          ></v-text-field>
-
-          <v-card-action>
-            <router-link
-              to="/reset-password"
-              class="
-                app-link
-                text-decoration-none
-                primary--text
-                font-weight-medium
-                d-inline-block
-              "
-              replace
-              >Mot de passe oublié</router-link
-            >
-            <v-btn
-              :disabled="!valid"
-              color="success"
-              class="mr-4"
-              @click="validate"
-              >Se connecter</v-btn
-            >
-          </v-card-action>
-        </v-form>
-      </v-container>
-    </v-card-text>
-  </v-card>
-</template>
 
 <script>
 export default {
@@ -59,25 +5,11 @@ export default {
   components: {},
   data () {
     return {
-      valid: true,
-      email: '',
-      password: '',
-      passwordRules: [
-        (v) => !!v || 'Mot de passe requis',
-        (v) => v.length >= 4 || 'Mot de passe supérieur à 8 caractères'
-      ],
-      emailRules: [
-        (v) => !!v || 'E-mail requis',
-        (v) => /.+@.+/.test(v) || 'E-mail doit etre valide'
-      ]
+
     }
   },
   methods: {
-    validate () {
-      this.$refs.form.validate()
-      const payload = { email: this.email, password: this.password }
-      this.$store.dispatch('checkUserExist', payload)
-    }
+
   }
 }
 </script>
